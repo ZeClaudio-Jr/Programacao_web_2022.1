8f3bfd9b553921156a834c5330072dd262022ea6
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2022-04-27 23:34:55 -0300

    Map()

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,38 @@
 alert("Você optou por ver tabela de carros e motos");
 
+var carros = [{
+    marca: "Volkswagem",
+    modelo: "Jetta",
+    ano: 2020
+}
+{
+    marca: "Volkswagem",
+    modelo: "Fusca",
+    ano: 1996
+}
+{
+    marca: "Fiat",
+    modelo: "Strada Vulcano",
+    ano: 2022
+}
+{
+    marca: "Chevrolet",
+    modelo: "Vectra",
+    ano: 2005
+}
+{
+    marca: "Ford",
+    modelo: "F-250",
+    ano: 2010
+}
+{
+    marca: "BMW",
+    modelo: "320i",
+    ano: 2021
+}
+]
+
+var show = carros.map(function(lista));
+
+function lista()
+
