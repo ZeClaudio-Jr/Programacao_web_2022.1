c31955362408cbca08e242b8c442682e9d821035
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2022-04-27 23:39:41 -0300

    Concertando erros

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,27 @@ var carros = [{
     marca: "Volkswagem",
     modelo: "Jetta",
     ano: 2020
-}
+},
 {
     marca: "Volkswagem",
     modelo: "Fusca",
     ano: 1996
-}
+},
 {
     marca: "Fiat",
     modelo: "Strada Vulcano",
     ano: 2022
-}
+},
 {
     marca: "Chevrolet",
     modelo: "Vectra",
     ano: 2005
-}
+},
 {
     marca: "Ford",
     modelo: "F-250",
     ano: 2010
-}
+},
 {
     marca: "BMW",
     modelo: "320i",
@@ -32,7 +32,7 @@ var carros = [{
 }
 ]
 
-var show = carros.map(function(lista));
+var show = carros.map(function(lista))
 
-function lista()
+function lista(){}
 
