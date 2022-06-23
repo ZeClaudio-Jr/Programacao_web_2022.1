df7f7532f2432e500f6cdfc0a79bbe64f90bef7a
ZeClaudio-jr <zeclaudio_jr5@yahoo.com.br>
2022-06-23 01:32:40 -0300

    Adição de novos arquivos js

---
+++ next/pages/cabecalho.js
@@ -0,0 +1,17 @@
+export default function Cabecalho(){
+
+    return (
+ 
+       <div>
+ 
+          <h1>
+ 
+            Minha Madrinha amada..
+ 
+          </h1>
+ 
+       </div>
+ 
+    )
+ 
+ }
---
+++ next/pages/dataAtual.js
@@ -0,0 +1,23 @@
+export default function dataAtual() {
+
+    return (
+
+        <div>
+
+        var data = new Date();
+
+        var dia = String(data.getDate()).padStart(2, '0');
+
+        var mes = String(data.getMonth() + 1).padStart(2, '0');
+
+        var ano = data.getFullYear();
+
+        dataAtual = dia + '/' + mes + '/' + ano;
+
+        console.log(dataAtual);
+
+        </div>
+
+    )
+
+}
