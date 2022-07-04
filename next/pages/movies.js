06d1fe18d069f5d03f3bbb8faaea42d102b86d57
ZeClaudio-Jr <77695149+ZeClaudio-Jr@users.noreply.github.com>
2022-07-04 10:55:34 -0300

    Update movies.js

--- next/pages/movies.js
+++ next/pages/movies.js
@@ -1,101 +1,113 @@
-export default function Movies(){
+export default function Movies() {
 
 
 
     const data = {
 
-  "Search": [
+        "Search": [
 
-    {
+            {
 
-      "Title": "Bagdad Café",
+                "Title": "Bagdad Café",
 
-      "Year": "1987",
+                "Year": "1987",
 
-      "imdbID": "tt0095801",
+                "imdbID": "tt0095801",
 
-      "Type": "movie",
+                "Type": "movie",
 
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNmUwZGZhZTktNGNmOS00NDA0LWEzYWUtZjE2ODA5OTM2OTc2XkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_SX300.jpg"
+                "Poster": "https://m.media-amazon.com/images/M/MV5BNmUwZGZhZTktNGNmOS00NDA0LWEzYWUtZjE2ODA5OTM2OTc2XkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_SX300.jpg"
 
-    },
+            },
 
-    {
+            {
 
-      "Title": "The Thief of Bagdad",
+                "Title": "The Thief of Bagdad",
 
-      "Year": "1940",
+                "Year": "1940",
 
-      "imdbID": "tt0033152",
+                "imdbID": "tt0033152",
 
-      "Type": "movie",
+                "Type": "movie",
 
-      "Poster": "https://m.media-amazon.com/images/M/MV5BZWFhYjg4NTEtY2IzMS00YTc2LTg1NGUtMTEzNDBlZDIxZTk3XkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_SX300.jpg"
+                "Poster": "https://m.media-amazon.com/images/M/MV5BZWFhYjg4NTEtY2IzMS00YTc2LTg1NGUtMTEzNDBlZDIxZTk3XkEyXkFqcGdeQXVyMTMxMTY0OTQ@._V1_SX300.jpg"
 
-    },
+            },
 
-    {
+            {
 
-      "Title": "Bagdad",
+                "Title": "Bagdad",
 
-      "Year": "1949",
+                "Year": "1949",
 
-      "imdbID": "tt0041149",
+                "imdbID": "tt0041149",
 
-      "Type": "movie",
+                "Type": "movie",
 
-      "Poster": "https://m.media-amazon.com/images/M/MV5BZjAxYTJmMmItYzZhMS00OWMwLTkxMjEtZjVmNmM3ZGRlZTljXkEyXkFqcGdeQXVyNTk1MTk0MDI@._V1_SX300.jpg"
+                "Poster": "https://m.media-amazon.com/images/M/MV5BZjAxYTJmMmItYzZhMS00OWMwLTkxMjEtZjVmNmM3ZGRlZTljXkEyXkFqcGdeQXVyNTk1MTk0MDI@._V1_SX300.jpg"
 
-    },
+            },
 
-    {
+            {
 
-      "Title": "Bowery to Bagdad",
+                "Title": "Bowery to Bagdad",
 
-      "Year": "1955",
+                "Year": "1955",
 
-      "imdbID": "tt0044388",
+                "imdbID": "tt0044388",
 
-      "Type": "movie",
+                "Type": "movie",
 
-      "Poster": "https://m.media-amazon.com/images/M/MV5BZTMwMTUyZTktOTk4OC00ZGY2LWFmMzAtYjkxMWMxNjdmOTg4XkEyXkFqcGdeQXVyNDY3MzU2MDM@._V1_SX300.jpg"
+                "Poster": "https://m.media-amazon.com/images/M/MV5BZTMwMTUyZTktOTk4OC00ZGY2LWFmMzAtYjkxMWMxNjdmOTg4XkEyXkFqcGdeQXVyNDY3MzU2MDM@._V1_SX300.jpg"
 
-    },
+            },
 
-    {
+            {
 
-      "Title": "Babes in Bagdad",
+                "Title": "Babes in Bagdad",
 
-      "Year": "1952",
+                "Year": "1952",
 
-      "imdbID": "tt0044389",
+                "imdbID": "tt0044389",
 
-      "Type": "movie",
+                "Type": "movie",
 
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYjBmNmRlNzAtZDNjYi00ZjQ0LTljYTktZGUxOGNhMjFhZWI1XkEyXkFqcGdeQXVyMTE2NzA0Ng@@._V1_SX300.jpg"
+                "Poster": "https://m.media-amazon.com/images/M/MV5BYjBmNmRlNzAtZDNjYi00ZjQ0LTljYTktZGUxOGNhMjFhZWI1XkEyXkFqcGdeQXVyMTE2NzA0Ng@@._V1_SX300.jpg"
+
+            }
+
+        ],
+
+        "totalResults": "59",
+
+        "Response": "True"
 
     }
 
-  ],
-
-  "totalResults": "59",
-
-  "Response": "True"
-
-}
-
     return (
 
+        // <div>
+
+        //     {data.Search.map((m) => <div>{m.Title} --- {m.Year}</div>)}
+
+        // </div>
+
         <div>
-
-            <div>
-
-                {data.Search[0].Title} --- {data.Search[0].Year}                
-
-            </div>
-
+            {data.Search.map(transformar)}
         </div>
 
     )
 
+
+
+}
+
+
+function transformar (elemento) {
+
+    return (
+        <div>
+            {elemento.Title} --- {elemento.Year}
+        </div>
+    )
 }
