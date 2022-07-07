200369049edc1dbd5a956460f28b27894bb2a544
ZeClaudio-Jr <77695149+ZeClaudio-Jr@users.noreply.github.com>
2022-07-07 10:22:35 -0300

    Add files via upload

---
+++ next/pages/movies2.js
@@ -0,0 +1,72 @@
+// export default function Movies() {
+
+//     <div>
+
+//         <div>
+
+//             {data.Search.map((m) => <div>{m.Title}   - - -  {m.Year} <img src={m.poster} width="220px" /></div>)}
+
+
+//         </div>
+
+
+//     </div>
+
+ 
+// export async function getServerSideProps(context) {
+//     const res = await fetch(`http://www.omdbapi.com/?apikey=c8f0306e&s=bagdad`)
+//     const data = await res.json()
+//     return {
+//         props: {
+
+//             data
+
+//         }
+//     }
+// }
+
+
+// async function fetcher(url) {
+
+//     const res = await fetch(url)
+
+//     const json = await res.json()
+
+//     return json
+
+// }
+
+
+import useSWR from 'swr'
+
+export default function Movies2(){
+
+    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=c8f0306e&s=bagdad`, fetcher)    
+
+    if (error) return <div>falha na requisição...</div>
+
+    if (!data) return <div>carregando...</div>
+
+    return (
+
+        <div>
+
+            { data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  ) }
+
+        </div>
+
+    )    
+
+}
+
+
+
+async function fetcher(url) {
+
+    const res = await fetch(url);
+
+    const json = await res.json();
+
+    return json;
+
+}
