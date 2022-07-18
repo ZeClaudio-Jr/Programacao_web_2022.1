ec774aa9a87e7fb7a691c12d6ae4b5f75c82d7f2
ZeClaudio_jr <zeclaudio_jr5@yahoo.com.br>
2022-07-17 21:12:45 -0300

    Adição do movies33.js

---
+++ next/pages/movies33.js
@@ -0,0 +1,19 @@
+export function TheForm(){
+
+    return (
+
+        <div>
+
+            <form>
+
+                <label htmlFor="titleSearchString">Filtro de Título</label>
+
+                <input id="titleSearchString" name="titleSearchString" type="text" autoComplete="true"/>
+
+            </form>
+
+        </div>
+
+    )
+
+}
